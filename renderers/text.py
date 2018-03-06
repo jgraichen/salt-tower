a79027c474d74a36051189698dd074f918768931
Jan Graichen <jgraichen@altimos.de>
2018-03-06 23:37:37 +0100

    Add additional text and yamlet renderer
    This commit adds two new renderers that work in conjunction with the tower
    pillar extensions:
    
    The text renderer allows to load and preprocess text blobs as templates. It
    can be used to e.g. load configuration files are pillar objects.
    
    The yamlet renderer is an improved YAML renderer that supports additional tags
    to e.g. read (binary) files or include rendered templates into the resulting
    data structure. This can be used to load e.g. binary certificate files or
    renderer configuration files into specific pillar keys without format issues.

---
+++ renderers/text.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+'''
+A text renderer for processing and loading text blobs.
+
+It is mainly developed to be used with file loaded in tower pillars and to be
+used with the yamlet renderers include tag:
+
+.. code-block:: yaml
+
+    pillar:
+      key: !include ./file.txt
+
+.. code-block:: text
+
+    #!text
+
+    The pillar key content.
+
+
+The following example will render a text file and
+strip whitespace from both ends:
+
+.. code-block:: text
+
+    #!text strip
+
+    A short text message
+
+
+The renderer is further able to wrap the text blob into a nested dictionary:
+
+.. code-block:: text
+
+    #!text strip key=path:to:blob
+
+    A short text message
+
+
+This will return the following python dict:
+
+.. code-block:: python
+
+    {'path': {'to': {'blob': 'A short text message'}}}
+
+'''
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+from salt.ext.six import string_types
+
+
+def render(blob, saltenv='', sls='', argline=None, key=None, **kwargs):
+    if not isinstance(blob, string_types):
+        blob = blob.read()
+
+    if blob.startswith('#!'):
+        blob = blob[(blob.find('\n') + 1):]
+
+    if argline is not None:
+        for arg in argline.split(None):
+            if '=' in arg:
+                k, v = arg.split('=', 1)
+            else:
+                k, v = arg, None
+
+            if k == 'strip':
+                blob = blob.strip()
+            elif k == 'key':
+                key = v
+
+    if key:
+        for k in reversed(key.split(':')):
+            blob = {k: blob}
+
+    return blob
---
+++ renderers/yamlet.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+'''
+Extended YAML renderer for salt.
+
+For YAML usage information see :ref:`Understanding YAML <yaml>`.
+
+Special extensions are added to
+
+* include other render files at arbitrary positions
+* include plain or binary files at arbitrary positions
+
+'''
+from __future__ import absolute_import
+
+import io
+import logging
+import os
+import warnings
+from yaml.constructor import ConstructorError
+from yaml.nodes import ScalarNode, MappingNode
+
+
+import salt.loader
+from salt.utils.yamlloader import SaltYamlSafeLoader, load
+from salt.utils.odict import OrderedDict
+from salt.ext.six import string_types
+from salt.exceptions import SaltRenderError
+
+
+log = logging.getLogger(__name__)
+
+
+class YamletLoader(SaltYamlSafeLoader):
+    def __init__(self, stream, **kwargs):
+        SaltYamlSafeLoader.__init__(self, stream, dictclass=OrderedDict)
+
+        self.kwargs = kwargs
+        self.add_constructor(u'!read', type(self)._yamlet_read)
+        self.add_constructor(u'!include', type(self)._yamlet_include)
+
+    def _yamlet_read(self, node):
+        if isinstance(node, ScalarNode):
+            return self._read(node.value)
+        else:
+            self._invalid_node(node, 'a scalar node')
+
+
+    def _yamlet_include(self, node):
+        if isinstance(node, ScalarNode):
+            return self._compile(node.value)
+        elif isinstance(node, MappingNode):
+            return self._compile(**self.construct_mapping(node, True))
+        else:
+            self._invalid_node(node, 'a scalar or mapping node')
+
+    def _read(self, source):
+        source = self._resolve(source)
+
+        with salt.utils.fopen(source, 'rb') as f:
+            return f.read()
+
+    def _compile(self, source, default='jinja|yamlet', context=None):
+        source = self._resolve(source)
+
+        kwargs = {}
+        kwargs.update(self.kwargs)
+        kwargs['context'] = {}
+
+        if 'context' in self.kwargs:
+            kwargs['context'].update(self.kwargs['context'])
+
+        if context:
+            kwargs['context'].update(context)
+
+        ret = salt.template.compile_template(
+            template=source,
+            default=default,
+            blacklist='',
+            whitelist='',
+            **kwargs)
+
+        if isinstance(ret, io.IOBase):
+            ret = ret.read()
+
+        return ret
+
+    def _resolve(self, path):
+        if 'tmplpath' in self.kwargs:
+            base = os.path.dirname(self.kwargs['tmplpath'])
+
+            return os.path.realpath(os.path.join(base, path))
+
+        log.warn(self.kwargs['tmplpath'])
+
+        return path
+
+    def _invalid_node(self, node, expected):
+        raise ConstructorError(
+            None,
+            None,
+            'expected {0}, but found {1}'.format(expected, node.id),
+            node.start_mark)
+
+
+
+def get_yaml_loader(**kwargs):
+    def yaml_loader(*args):
+        return YamletLoader(*args, **kwargs)
+
+    return yaml_loader
+
+
+def render(source, saltenv='base', sls='', argline='', **kwargs):
+    '''
+    Processes YAML data in a string or file objects.
+
+    :rtype: A Python data structure
+    '''
+    if not isinstance(source, string_types):
+        source = source.read()
+
+    return load(source, Loader=get_yaml_loader(**kwargs))
