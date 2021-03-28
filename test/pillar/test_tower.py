4c73b11ac06748f11ab3a53315c0bb18b03008bb
Jan Graichen <jgraichen@altimos.de>
2021-03-28 13:21:48 +0200

    Format code with black
    Consistent formatting and easy to auto-apply

--- test/pillar/test_tower.py
+++ test/pillar/test_tower.py
@@ -9,381 +9,374 @@ from salt.exceptions import SaltRenderError
 
 
 def test_common_wildcard(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - common/*
-            ''',
-        'common/test.sls':
-            '''
+            """,
+            "common/test.sls": """
             common: True
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'common': True}
+    assert env.ext_pillar() == {"common": True}
 
 
 def test_jinja(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - {{ grains['id'] }}.sls
-            ''',
-        'test_master.sls':
-            '''
+            """,
+            "test_master.sls": """
             minion:
                 id: {{ grains['id'] }}
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'minion': {'id': 'test_master'}}
+    assert env.ext_pillar() == {"minion": {"id": "test_master"}}
 
 
 def test_match_minion_id(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - test_*:
                     - match: minion_id
-            '''
-    })
+            """
+        }
+    )
 
-    assert env.ext_pillar() == {'match': 'minion_id'}
+    assert env.ext_pillar() == {"match": "minion_id"}
 
 
 def test_match_extended_regex(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - E@[tes]{4}_master$:
                     - match: regexp
-            '''
-    })
+            """
+        }
+    )
 
-    assert env.ext_pillar() == {'match': 'regexp'}
+    assert env.ext_pillar() == {"match": "regexp"}
 
 
 def test_match_pillar_data(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - I@pillar_key:True:
                     - match: pillar
-            '''
-    })
-
-    assert env.ext_pillar(pillar={'pillar_key': True}) == {
-            'pillar_key': True,
-            'match': 'pillar'
+            """
         }
+    )
+
+    assert env.ext_pillar(pillar={"pillar_key": True}) == {
+        "pillar_key": True,
+        "match": "pillar",
+    }
 
 
 def test_match_tower_pillar_key(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - '*':
                     - minion: {id: 'value'}
                 - I@minion:id:*:
                     - match: tower_pillar
-            '''
-    })
-
-    assert env.ext_pillar() == {
-            'minion': {'id': 'value'},
-            'match': 'tower_pillar'
+            """
         }
+    )
+
+    assert env.ext_pillar() == {"minion": {"id": "value"}, "match": "tower_pillar"}
 
 
 def test_match_grains(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - G@virtual:*:
                     - match: grains
-            '''
-    })
+            """
+        }
+    )
 
-    assert env.ext_pillar() == {'match': 'grains'}
+    assert env.ext_pillar() == {"match": "grains"}
 
 
 @pytest.mark.skip(reason="Global format currently disable as it's a high-risk feature")
 def test_late_bind(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - '*':
                     - minion: {id: 'my-minion-id'}
                     - late_bind: {minion_id: '{minion.id}'}
-            '''
-    })
+            """
+        }
+    )
 
     assert env.ext_pillar() == {
-            'minion': {'id': 'my-minion-id'},
-            'late_bind': {'minion_id': 'my-minion-id'}
-        }
+        "minion": {"id": "my-minion-id"},
+        "late_bind": {"minion_id": "my-minion-id"},
+    }
 
 
 def test_late_bind_not_matching(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - '*':
                     - late_bind: '{fuubar}'
-            '''
-    })
-
-    assert env.ext_pillar() == {
-            'late_bind': '{fuubar}'
+            """
         }
+    )
+
+    assert env.ext_pillar() == {"late_bind": "{fuubar}"}
 
 
 def test_late_bind_invalid(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - '*':
                     - late_bind: "{^$://DJF$$}"
-            '''
-    })
-
-    assert env.ext_pillar() == {
-            'late_bind': '{^$://DJF$$}'
+            """
         }
+    )
+
+    assert env.ext_pillar() == {"late_bind": "{^$://DJF$$}"}
 
 
 def test_default_renderers(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - pillar.sls
-            ''',
-        'pillar.sls':
-            '''
+            """,
+            "pillar.sls": """
             yaml: 'yes'
             jinja: '{{ "yes" }}'
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'yaml': 'yes', 'jinja': 'yes'}
+    assert env.ext_pillar() == {"yaml": "yes", "jinja": "yes"}
 
 
 def test_renderers_shebang_py(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - pillar.sls
-            ''',
-        'pillar.sls':
-            '''
+            """,
+            "pillar.sls": """
             #!py
 
             def run():
                 return {'py': 'yes'}
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'py': 'yes'}
+    assert env.ext_pillar() == {"py": "yes"}
 
 
 def test_include(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - common
-            ''',
-        'common.sls':
-            '''
+            """,
+            "common.sls": """
             include:
                 - else.sls
-            ''',
-        'else.sls':
-            '''
+            """,
+            "else.sls": """
             else: True
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'else': True}
+    assert env.ext_pillar() == {"else": True}
 
 
 def test_include_relative(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - path/to/file
-            ''',
-        'path/to/file.sls':
-            '''
+            """,
+            "path/to/file.sls": """
             include:
                 - ./else.sls
-            ''',
-        'path/to/else.sls':
-            '''
+            """,
+            "path/to/else.sls": """
             else: True
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'else': True}
+    assert env.ext_pillar() == {"else": True}
 
 
 def test_include_relative_up(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - path/to/file
-            ''',
-        'path/to/file.sls':
-            '''
+            """,
+            "path/to/file.sls": """
             include:
                 - ../else.sls
-            ''',
-        'path/else.sls':
-            '''
+            """,
+            "path/else.sls": """
             else: True
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'else': True}
+    assert env.ext_pillar() == {"else": True}
 
 
 def test_include_nested(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - a/common
-            ''',
-        'a/common.sls':
-            '''
+            """,
+            "a/common.sls": """
             include:
                 - b/file.sls
-            ''',
-        'b/file.sls':
-            '''
+            """,
+            "b/file.sls": """
             test: True
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'test': True}
+    assert env.ext_pillar() == {"test": True}
 
 
 def test_include_nested_init(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - a/common
-            ''',
-        'a/common.sls':
-            '''
+            """,
+            "a/common.sls": """
             include:
                 - b
-            ''',
-        'b/init.sls':
-            '''
+            """,
+            "b/init.sls": """
             test: True
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'test': True}
+    assert env.ext_pillar() == {"test": True}
 
 
 def test_include_nested_glob(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - a/common
-            ''',
-        'a/common.sls':
-            '''
+            """,
+            "a/common.sls": """
             include:
                 - b/*.sls
-            ''',
-        'b/one.sls':
-            '''
+            """,
+            "b/one.sls": """
             one: True
-            ''',
-        'b/two.sls':
-            '''
+            """,
+            "b/two.sls": """
             two: True
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'one': True, 'two': True}
+    assert env.ext_pillar() == {"one": True, "two": True}
 
 
 def test_include_recursive(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - common
-            ''',
-        'common.sls':
-            '''
+            """,
+            "common.sls": """
             include: [else.sls]
             order: [1]
-            ''',
-        'else.sls':
-            '''
+            """,
+            "else.sls": """
             include: [common.sls]
             order: [2]
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'order': [2, 1]}
+    assert env.ext_pillar() == {"order": [2, 1]}
 
 
 def test_context_tmpl(env, tmpdir):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - template.sls
-            ''',
-        'template.sls':
-            '''
+            """,
+            "template.sls": """
             tmplpath: {{ tmplpath }}
             tmpldir: {{ tmpldir }}
-            '''
-    })
+            """,
+        }
+    )
 
     assert env.ext_pillar() == {
-            'tmplpath': os.path.join(tmpdir, 'template.sls'),
-            'tmpldir': tmpdir
-        }
+        "tmplpath": os.path.join(tmpdir, "template.sls"),
+        "tmpldir": tmpdir,
+    }
 
 
 def test_context_minion_id(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - template.sls
-            ''',
-        'template.sls':
-            '''
+            """,
+            "template.sls": """
             minion_id: {{ minion_id }}
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'minion_id': 'test_master'}
+    assert env.ext_pillar() == {"minion_id": "test_master"}
 
 
 def test_context_basedir(env):
@@ -392,23 +385,22 @@ def test_context_basedir(env):
 
     The !include tag should lookup non-relative paths from the tower root.
     """
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - common/init.sls
-            ''',
-        'common/init.sls':
-            '''
+            """,
+            "common/init.sls": """
             conf: !include {{ basedir }}/files/conf.txt
-            ''',
-        'files/conf.txt':
-            '''
+            """,
+            "files/conf.txt": """
             Some config file...
-            '''
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'conf': 'Some config file...'}
+    assert env.ext_pillar() == {"conf": "Some config file..."}
 
 
 def test_error_invalid_file(env):
@@ -416,107 +408,101 @@ def test_error_invalid_file(env):
     Rendering errors shall not be ignored but raised. Otherwise an incomplete or
     wrong pillar dataset might be returned and the error would go unnoticed.
     """
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - common/init.sls
-            ''',
-        'common/init.sls':
-            '''
+            """,
+            "common/init.sls": """
             {% abc %}
-            ''',
-    })
+            """,
+        }
+    )
 
     with pytest.raises(SaltRenderError):
         env.ext_pillar()
 
 
 def test_render_func(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - index
-            ''',
-        'index.sls':
-            '''
+            """,
+            "index.sls": """
             key: !include test/conf.j2
-            ''',
-        'test/conf.j2':
-            '''
+            """,
+            "test/conf.j2": """
             #! jinja | text strip
             {{ render('test/conf2.j2') }}
-            ''',
-        'test/conf2.j2':
-            '''
+            """,
+            "test/conf2.j2": """
             This is a test.
-            ''',
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'key': 'This is a test.'}
+    assert env.ext_pillar() == {"key": "This is a test."}
 
 
 def test_render_func_relative(env):
     """
     Relative paths must be specified using the `tmpldir` variable.
     """
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - index
-            ''',
-        'index.sls':
-            '''
+            """,
+            "index.sls": """
             key: !include test/conf.j2
-            ''',
-        'test/conf.j2':
-            '''
+            """,
+            "test/conf.j2": """
             #! jinja | text strip
             {{ render(tmpldir + '/conf2.j2') }}
-            ''',
-        'test/conf2.j2':
-            '''
+            """,
+            "test/conf2.j2": """
             This is a test.
-            ''',
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'key': 'This is a test.'}
+    assert env.ext_pillar() == {"key": "This is a test."}
 
 
 def test_render_func_in_top(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - '*':
                     - key: !include test/conf.j2
-            ''',
-        'test/conf.j2':
-            '''
+            """,
+            "test/conf.j2": """
             #! jinja | text strip
             {{ render(tmpldir + '/conf2.j2') }}
-            ''',
-        'test/conf2.j2':
-            '''
+            """,
+            "test/conf2.j2": """
             This is a test.
-            ''',
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'key': 'This is a test.'}
+    assert env.ext_pillar() == {"key": "This is a test."}
 
 
 def test_render_func_context(env):
-    env.setup({
-        'tower.sls':
-            '''
+    env.setup(
+        {
+            "tower.sls": """
             base:
                 - '*':
                     - key: !include context/test.py
-            ''',
-        'context/test.py':
-            '''
+            """,
+            "context/test.py": """
             #!py
             import os.path
             def run():
@@ -524,12 +510,12 @@ def test_render_func_context(env):
                     os.path.join(context["tmplpath"], "../template.j2"),
                     context={**context, "message": "Hello World!"}
                 )
-            ''',
-        'context/template.j2':
-            '''
+            """,
+            "context/template.j2": """
             #!jinja|text strip
             {{ message }}
-            ''',
-    })
+            """,
+        }
+    )
 
-    assert env.ext_pillar() == {'key': 'Hello World!'}
+    assert env.ext_pillar() == {"key": "Hello World!"}
--- test/pillar/test_tower_tower.py
+++ test/pillar/test_tower_tower.py
@@ -10,132 +10,143 @@ from test.conftest import __pillars__
 
 @pytest.fixture
 def mod():
-    return inspect.getmodule(__pillars__['tower'])
+    return inspect.getmodule(__pillars__["tower"])
 
 
 @pytest.fixture
 def tower(mod):
-    return mod.Tower('minion-id', 'base', {
-        'pillar': {
-            'key': 'value',
-            'list': [1, 2]
-        }
-    })
+    return mod.Tower(
+        "minion-id",
+        "base",
+        {
+            "pillar": {
+                "key": "value",
+                "list": [1, 2],
+            }
+        },
+    )
 
 
 def test_get_traverse(tower):
-    assert tower.get('pillar:key') == 'value'
+    assert tower.get("pillar:key") == "value"
+
 
 def test_get_default(tower):
-    assert tower.get('pillar:none') == None
+    assert tower.get("pillar:none") == None
+
 
 def test_get_default_value(tower):
-    assert tower.get('pillar:none', default=5) == 5
+    assert tower.get("pillar:none", default=5) == 5
+
 
 def test_get_default_require(tower):
     with pytest.raises(KeyError):
-        tower.get('pillar:none', require=True)
+        tower.get("pillar:none", require=True)
+
 
 def test_update(tower):
-    tower.update({
-        'pillar': {
-            'key': 'new_value',
-            'list': [3]
+    tower.update(
+        {
+            "pillar": {
+                "key": "new_value",
+                "list": [3],
+            }
         }
-    })
+    )
 
     assert tower == {
-        'pillar': {
-            'key': 'new_value',
-            'list': [1, 2, 3]
+        "pillar": {
+            "key": "new_value",
+            "list": [1, 2, 3],
         }
     }
 
-def test_update_override(tower):
-    '''
-    Merging different types will override
-    '''
-    tower.update({'pillar': {'list': 'fake'}})
 
-    assert tower['pillar']['list'] == 'fake'
+def test_update_override(tower):
+    """
+    Merging different types will override
+    """
+    tower.update({"pillar": {"list": "fake"}})
+
+    assert tower["pillar"]["list"] == "fake"
 
 
 def test_merge(tower):
-    tgt = {'a': True, 'b': [1]}
-    ret = tower.merge(tgt, {'b': [2]})
+    tgt = {"a": True, "b": [1]}
+    ret = tower.merge(tgt, {"b": [2]})
 
     assert ret is tgt
-    assert ret == {'a': True, 'b': [1, 2]}
+    assert ret == {"a": True, "b": [1, 2]}
 
 
 def test_merge_copy(tower):
     tgt = {}
-    mod = {'a': {'b': 2}}
+    mod = {"a": {"b": 2}}
 
     tower.merge(tgt, mod)
 
-    assert tgt == {'a': {'b': 2}}
-    assert tgt['a'] is not mod['a']
+    assert tgt == {"a": {"b": 2}}
+    assert tgt["a"] is not mod["a"]
 
 
 def test_merge_copy_list(tower):
-    tgt = [{'a': 1}]
-    mod = [{'b': 2}]
+    tgt = [{"a": 1}]
+    mod = [{"b": 2}]
 
     tower.merge(tgt, mod)
 
-    assert tgt == [{'a': 1}, {'b': 2}]
+    assert tgt == [{"a": 1}, {"b": 2}]
     assert tgt[1] is not mod[0]
 
 
 def test_merge_list_strategy_remove(tower):
-    tgt = ['a', 'b']
-    mod = [{'__': 'remove'}, 'a']
+    tgt = ["a", "b"]
+    mod = [{"__": "remove"}, "a"]
 
     tower.merge(tgt, mod)
 
-    assert tgt == ['b']
+    assert tgt == ["b"]
 
 
 def test_merge_list_strategy_remove_non_existant(tower):
-    tgt = ['a', 'b']
-    mod = [{'__': 'remove'}, 'c']
+    tgt = ["a", "b"]
+    mod = [{"__": "remove"}, "c"]
 
     tower.merge(tgt, mod)
 
-    assert tgt == ['a', 'b']
+    assert tgt == ["a", "b"]
 
 
 def test_merge_list_strategy_merge_first(tower):
-    tgt = ['a', 'b']
-    mod = [{'__': 'merge-first'}, 'c']
+    tgt = ["a", "b"]
+    mod = [{"__": "merge-first"}, "c"]
 
     tower.merge(tgt, mod)
 
-    assert tgt == ['c', 'a', 'b']
+    assert tgt == ["c", "a", "b"]
 
 
 def test_merge_list_strategy_merge_overwrite(tower):
-    tgt = ['a', 'b']
-    mod = [{'__': 'overwrite'}, 'c']
+    tgt = ["a", "b"]
+    mod = [{"__": "overwrite"}, "c"]
 
     tower.merge(tgt, mod)
 
-    assert tgt == ['c']
+    assert tgt == ["c"]
 
 
 def test_merge_list_remove_nested_doubledash(tower):
-    tgt = {'a': 0}
-    mod = {'a': [{'__': 'overwrite'}, 'a', 'b']}
+    tgt = {"a": 0}
+    mod = {"a": [{"__": "overwrite"}, "a", "b"]}
 
     tower.merge(tgt, mod)
 
-    assert tgt == {'a': ['a', 'b']}
+    assert tgt == {"a": ["a", "b"]}
 
 
 def test_merge_list_remove_nested_doubledash_list(tower):
     tgt = [0]
-    mod = [[{'__': 'overwrite'}, 1]]
+    mod = [[{"__": "overwrite"}, 1]]
 
     tower.merge(tgt, mod)
 
@@ -143,74 +154,73 @@ def test_merge_list_remove_nested_doubledash_list(tower):
 
 
 def test_merge_dict_strategy_remove(tower):
-    tgt = {'a': 0, 'b': 1}
-    mod = {'__': 'remove', 'a': None}
+    tgt = {"a": 0, "b": 1}
+    mod = {"__": "remove", "a": None}
 
     tower.merge(tgt, mod)
 
-    assert tgt == {'b': 1}
+    assert tgt == {"b": 1}
 
 
 def test_merge_dict_strategy_remove_non_existant(tower):
-    tgt = {'a': 0, 'b': 1}
-    mod = {'__': 'remove', 'c': None}
+    tgt = {"a": 0, "b": 1}
+    mod = {"__": "remove", "c": None}
 
     tower.merge(tgt, mod)
 
-    assert tgt == {'a': 0, 'b': 1}
+    assert tgt == {"a": 0, "b": 1}
 
 
 def test_merge_dict_strategy_merge_first(tower):
-    tgt = {'a': 0, 'b': 1, 'd': [4]}
-    mod = {'__': 'merge-first', 'a': 1, 'c': 2, 'd': [5]}
+    tgt = {"a": 0, "b": 1, "d": [4]}
+    mod = {"__": "merge-first", "a": 1, "c": 2, "d": [5]}
 
     tower.merge(tgt, mod)
 
-    assert tgt == {'a': 0, 'b': 1, 'c': 2, 'd': [5, 4]}
+    assert tgt == {"a": 0, "b": 1, "c": 2, "d": [5, 4]}
 
 
 def test_merge_dict_strategy_merge_overwrite(tower):
-    tgt = {'a': 0, 'b': 1}
-    mod = {'__': 'overwrite', 'c': 2}
+    tgt = {"a": 0, "b": 1}
+    mod = {"__": "overwrite", "c": 2}
 
     tower.merge(tgt, mod)
 
-    assert tgt == {'c': 2}
+    assert tgt == {"c": 2}
 
 
 def test_merge_dict_remove_nested_doubledash(tower):
-    tgt = {'a': 0}
-    mod = {'a': {'__': 'overwrite', 'key': 1}}
+    tgt = {"a": 0}
+    mod = {"a": {"__": "overwrite", "key": 1}}
 
     tower.merge(tgt, mod)
 
-    assert tgt == {'a': {'key': 1}}
+    assert tgt == {"a": {"key": 1}}
 
 
 def test_format(tower):
-    tower.update({'app': {'name': 'MyApp'}})
+    tower.update({"app": {"name": "MyApp"}})
 
-    assert tower.format('X{app.name}X') == 'XMyAppX'
+    assert tower.format("X{app.name}X") == "XMyAppX"
 
 
 def test_format_bytes(tower):
-    tower.update({'app': {'name': 'MyApp'}})
+    tower.update({"app": {"name": "MyApp"}})
 
-    assert tower.format(b'X{app.name}X') == b'X{app.name}X'
+    assert tower.format(b"X{app.name}X") == b"X{app.name}X"
 
 
 def test_format_dict(tower):
-    tower.update({'ports': [80, 443]})
+    tower.update({"ports": [80, 443]})
 
-    assert tower.format({'bind': '0.0.0.0:{ports.0}'}) \
-        == {'bind': '0.0.0.0:80'}
+    assert tower.format({"bind": "0.0.0.0:{ports.0}"}) == {"bind": "0.0.0.0:80"}
 
 
 def test_format_list(tower):
-    tower.update({'ports': [80, 443]})
+    tower.update({"ports": [80, 443]})
 
-    assert tower.format(['{ports.0}', '{ports.1}']) == ['80', '443']
+    assert tower.format(["{ports.0}", "{ports.1}"]) == ["80", "443"]
 
 
 def test_format_arg(tower):
-    assert tower.format({'app': '{name}'}, name='APP') == {'app': 'APP'}
+    assert tower.format({"app": "{name}"}, name="APP") == {"app": "APP"}
