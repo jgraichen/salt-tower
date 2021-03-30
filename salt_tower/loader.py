2fc1e2c76ad23c2b59c34b6f6c92d7c8407e1a7c
Jan Graichen <jgraichen@altimos.de>
2021-03-30 17:08:51 +0200

    Format file with black

--- salt_tower/loader.py
+++ salt_tower/loader.py
@@ -7,7 +7,8 @@ PKG_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 def pillar_dirs():
-    yield os.path.join(PKG_DIR, 'pillar')
+    yield os.path.join(PKG_DIR, "pillar")
+
 
 def renderers_dirs():
-    yield os.path.join(PKG_DIR, 'renderers')
+    yield os.path.join(PKG_DIR, "renderers")
