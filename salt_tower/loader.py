1ed2b86dc83605fe5ee27765e3d96ac28258a9b2
Jan Graichen <jgraichen@altimos.de>
2021-04-03 13:07:49 +0200

    Change loader functions to work with salt 3003
    Salt 3003 changed their implemention to load extensions via python
    packages. The previous code iterated the result from loader functions,
    but the new code expects lists to be returned, simply ignoring anything
    else, including generators returned from generator functions.
    
    This change will return lists now, but all published packages will not
    load with Salt 3003 at all.
    
    See https://github.com/saltstack/salt/issues/59961.

--- salt_tower/loader.py
+++ salt_tower/loader.py
@@ -7,8 +7,8 @@ PKG_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 def pillar_dirs():
-    yield os.path.join(PKG_DIR, "pillar")
+    return [os.path.join(PKG_DIR, "pillar")]
 
 
 def renderers_dirs():
-    yield os.path.join(PKG_DIR, "renderers")
+    return [os.path.join(PKG_DIR, "renderers")]
